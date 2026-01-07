b4eae96b791c83203ab5d557a70ea85cf6e5b3b3
Gizzmonauta <gizzmonauta@hotmail.com>
2026-01-06 20:53:37 -0500

    feat: add fibonacci Big O(n)

--- reduction_to_p/fibonacci_reduced_to_p.py
+++ reduction_to_p/fibonacci_reduced_to_p.py
@@ -5,12 +5,24 @@ def fib(n):
         return 1
     grandparent = 0
     parent = 1
-    current = 0
     for i in range(n-1):
         current = parent + grandparent
         grandparent = parent
         parent = current
     return current
+
+def main():
+    print(f"Input : 1, Espected: 1 \nActual: {fib(1)}\n")
+    print(f"Input : 10, Espected: 55 \nActual: {fib(10)}\n")
+    print(f"Input : 20, Espected: 6765 \nActual: {fib(20)}\n")
+    print(f"Input : 0, Espected: 0 \nActual: {fib(0)}\n")
+    print(f"Input : 40, Espected: 102334155 \nActual: {fib(40)}\n")
+    print(f"Input : 70, Espected: 190392490709135 \nActual: {fib(70)}\n")
+    print(f"Input : 160, Espected: 1226132595394188293000174702095995 \nActual: {fib(160)}\n")
+
+if __name__ == "__main__":
+    main()
+
 
 # This is an inefficient recursive version for reference
 # It is exponentially slow for large n. it has a Big O(2^n) time complexity.
