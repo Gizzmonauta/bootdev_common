66259efd4645f6f56919f5cad8544a0930dd0d7f
Gizzmonauta <gizzmonauta@hotmail.com>
2026-01-10 16:33:26 -0500

    feat: add breadth_first_search method to Graph class and update tests for BFS functionality

--- greph_full.py
+++ greph_full.py
@@ -2,6 +2,22 @@ class Graph:
     def __init__(self):
         self.graph = {}
 
+    def breadth_first_search(self, v):
+        if v not in self.graph:
+            raise Exception("vertex not in graph")
+        visited_list = []
+        waiting_queue = []
+        waiting_queue.append(v)
+        while len(waiting_queue) > 0:
+            vis = waiting_queue[0]
+            del waiting_queue[0]
+            visited_list.append(vis)
+            sorted_list = sorted(self.graph[vis])
+            for sn in sorted_list:
+                if sn not in visited_list and sn not in waiting_queue:
+                    waiting_queue.append(sn)
+        return visited_list
+
     def unconnected_vertices(self):
         unconnected = []
         for vertex, connections in self.graph.items():
@@ -13,14 +29,14 @@ class Graph:
         return self.graph[node]
 
     def add_edge(self, u, v):
-        if u in self.graph:
+        if u in self.graph.keys():
             self.graph[u].add(v)
         else:
-            self.graph[u] = {v}
-        if v in self.graph:
+            self.graph[u] = set([v])
+        if v in self.graph.keys():
             self.graph[v].add(u)
         else:
-            self.graph[v] = {u} 
+            self.graph[v] = set([u])
 
     def add_node(self, u):
         if u not in self.graph:
@@ -30,6 +46,14 @@ class Graph:
         if u in self.graph and v in self.graph:
             return (v in self.graph[u]) and (u in self.graph[v])
         return False
+    
+    def __repr__(self):
+        result = ""
+        for key in self.graph.keys():
+            result += f"Vertex: '{key}'\n"
+            for v in sorted(self.graph[key]):
+                result += f"has an edge leading to --> {v} \n"
+        return result
 
 
 # Properties
