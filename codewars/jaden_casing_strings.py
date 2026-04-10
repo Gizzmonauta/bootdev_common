1813c65777f2de50bdd9b95dde4d523eb12df5e8
gizzmonauta <gizzmonauta@hotmail.com>
2026-04-09 23:35:02 -0500

    feat: add Jaden Casing Strings implementation with example usage

---
+++ codewars/jaden_casing_strings.py
@@ -0,0 +1,23 @@
+r"""
+Jaden Casing Strings
+
+Jaden Smith, the son of Will Smith, is the star of films such as The Karate Kid (2010) and After Earth (2013). Jaden is also known for some of his philosophy that he delivers via Twitter. When writing on Twitter, he is known for almost always capitalizing every word. For simplicity, you'll have to capitalize each word, check out how contractions are expected to be in the example below.
+
+Your task is to convert strings to how they would be written by Jaden Smith. The strings are actual quotes from Jaden Smith, but they are not capitalized in the same way he originally typed them.
+
+Example:
+
+Not Jaden-Cased: "How can mirrors be real if our eyes aren't real"
+Jaden-Cased:     "How Can Mirrors Be Real If Our Eyes Aren't Real"
+"""
+
+def to_jaden_case(string: str) -> str:
+    return ' '.join(word.capitalize() for word in string.split())
+
+def main():
+    print(to_jaden_case("How can mirrors be real if our eyes aren't real"), "How Can Mirrors Be Real If Our Eyes Aren't Real")
+    print(to_jaden_case("Most Trees Are Blue"), "Most Trees Are Blue")
+    print(to_jaden_case("All The Rules In This World Were Made By Someone No Smarter Than You And You Can Change Them"), "All The Rules In This World Were Made By Someone No Smarter Than You And You Can Change Them")
+
+if __name__ == '__main__':
+    main()
