910136b3072f9d33f7ea7342c9b5c4533b942057
gizzmonauta <gizzmonauta@hotmail.com>
2026-04-16 22:40:09 -0500

    feat: implement function to sum two lowest positive integers from an array

---
+++ codewars/sum_lowest_positive_integers.py
@@ -0,0 +1,20 @@
+r"""
+Sum of two lowest positive integers
+
+Create a function that returns the sum of the two lowest positive numbers given an array of minimum 4 positive integers. No floats or non-positive integers will be passed.
+
+For example, when an array is passed like [19, 5, 42, 2, 77], the output should be 7.
+
+[10, 343445353, 3453445, 3453545353453] should return 3453455.
+"""
+
+def sum_two_smallest_numbers(numbers: list[int]) -> int:
+    numbers.sort()
+    return numbers[0] + numbers[1]
+
+def main():
+    print(sum_two_smallest_numbers([19, 5, 42, 2, 77]), 7)
+    print(sum_two_smallest_numbers([10, 343445353, 3453445, 3453545353453]), 3453455)
+
+if __name__ == "__main__":
+    main()
