1e85106062219339e9f7418ff95aa5228f0fbc0d
gizzmonauta <gizzmonauta@hotmail.com>
2026-04-20 23:27:09 -0500

    feat: add function to find needle in a haystack and update main for testing

--- codewars/detect_pangram.py
+++ codewars/detect_pangram.py
@@ -30,15 +30,15 @@ def is_pangram_alt(s: str) -> bool:
     return True
 
 def main():
-    print(is_pangram("The quick brown fox jumps over the lazy dog."))
-    print(is_pangram("This is not a pangram."))
-    print(is_pangram("The quick brown fox jumps over the lazy dog."))
-    print(is_pangram("Cwm fjord bank glyphs vext quiz"))
-    print(is_pangram("Pack my box with five dozen liquor jugs."))
-    print(is_pangram("How quickly daft jumping zebras vex."))
-    print(is_pangram("ABCD45EFGH,IJK,LMNOPQR56STUVW3XYZ"))
-    print(is_pangram("This isn't a pangram!"))
-    print(is_pangram("abcdefghijklm opqrstuvwxyz"))
-    print(is_pangram("Aacdefghijklmnopqrstuvwxyz"))
+    print(is_pangram_alt("The quick brown fox jumps over the lazy dog."))
+    print(is_pangram_alt("This is not a pangram."))
+    print(is_pangram_alt("The quick brown fox jumps over the lazy dog."))
+    print(is_pangram_alt("Cwm fjord bank glyphs vext quiz"))
+    print(is_pangram_alt("Pack my box with five dozen liquor jugs."))
+    print(is_pangram_alt("How quickly daft jumping zebras vex."))
+    print(is_pangram_alt("ABCD45EFGH,IJK,LMNOPQR56STUVW3XYZ"))
+    print(is_pangram_alt("This isn't a pangram!"))
+    print(is_pangram_alt("abcdefghijklm opqrstuvwxyz"))
+    print(is_pangram_alt("Aacdefghijklmnopqrstuvwxyz"))
 if __name__ == "__main__":
     main()
